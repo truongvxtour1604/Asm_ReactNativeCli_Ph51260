c69822fd6967fba922eb4e3ae951005b6b575af8
truongvxtour1604 <truongvxtour1604@gmail.com>
2025-03-14 10:10:22 +0700

    create Splash, SignIn, SignUp screens

---
+++ screens/SignIn.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SignIn() {
+    return (
+        <View style={styles.container}>
+            <Text>Sign in</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    }
+});
+
+
---
+++ screens/SignUp.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SignUp() {
+    return (
+        <View style={styles.container}>
+            <Text>Sign up</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    }
+});
+
+
